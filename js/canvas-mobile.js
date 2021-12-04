b3e3e2edff055a1e83c6ab5e2fe9a2c04fc7fe71
Akhilesh.Verma <akhilesh.verma@student.ufv.ca>
2021-12-04 08:49:15 -0800

    changed page loader and animation code

--- js/canvas-mobile.js
+++ js/canvas-mobile.js
@@ -42,12 +42,12 @@ addEventListener("mousemove", (event) => {
   mouse.y = event.clientY;
 });
 
-addEventListener("resize", () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
+// addEventListener("resize", () => {
+//   canvas.width = innerWidth;
+//   canvas.height = innerHeight;
 
-  init();
-});
+//   init();
+// });
 
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -114,36 +114,38 @@ function init() {
       )
     );
   }
-  console.log("PC");
+  console.log("PC1");
 }
 
 function initMobile() {
   fix_dpi();
   particles = [];
 
-  for (let i = 0; i < 200; i++) {
-    const radius = Math.random() * 2 + 6;
+  for (let i = 0; i < 300; i++) {
+    const radius = Math.random() * 2 + 9;
     particles.push(
       new Particle(
         canvas.width / 3.5,
         canvas.height / 2,
         radius,
         randomColor(colors),
-        randomIntFromRange(100, 900)
+        randomIntFromRange(100, 1200)
         // "blue"
       )
     );
   }
-  console.log("mobile");
+  console.log("Mobile");
 }
 
 console.log(canvas.width * 2);
+console.log(canvas.width);
 console.log(canvas.width + canvas.width);
+
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
   // c.globalAlpha = 0.0;
-  c.fillStyle = "rgba(0,0,0,0.05";
+  c.fillStyle = "rgba(0,0,0,0.05)";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
   // c.fillText("HTML CANVAS BOILERPLATE", mouse.x, mouse.y);
@@ -163,229 +165,3 @@ animate();
 // -----------------------
 // --------------------------
 // --------------------------
-
-Letters = function () {
-  this.lettersDOM = null;
-  this.active = null;
-  this.letters = [];
-  this.alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "i",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "~",
-    "&",
-    "|",
-    "^",
-    "ç",
-    "@",
-    "]",
-    "[",
-    "{",
-    "}",
-    "ù",
-    "*",
-    "µ",
-    "¤",
-    "$",
-    "£",
-    "€",
-    "°",
-    ")",
-    "(",
-    "+",
-    "-",
-    "/",
-    "<",
-    ">",
-    "²",
-    "`",
-    "é",
-    "è",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-  ];
-
-  return this;
-};
-
-Letters.prototype.init = function (word) {
-  this.lettersDOM = document.querySelectorAll(".letter");
-  this.active = true;
-  var i;
-  var nextChar;
-  var lettersMax = this.lettersDOM.length;
-
-  for (i = 0; i < this.lettersDOM.length; i++) {
-    if (word.charAt(i) != "") nextChar = word.charAt(i);
-    else nextChar = false;
-
-    this.letters.push(new Letter(this.lettersDOM[i], nextChar));
-  }
-
-  if (word.length > lettersMax) {
-    var wordContainer = document.getElementById("word");
-
-    for (i = lettersMax; i < word.length; i++) {
-      var letterSpan = document.createElement("span");
-      letterSpan.innerHTML = "";
-      letterSpan.classList.add("letter");
-      wordContainer.appendChild(letterSpan);
-      this.letters.push(new Letter(letterSpan, word.charAt(i)));
-    }
-  }
-
-  this.animate();
-
-  return this;
-};
-
-Letters.prototype.animate = function () {
-  var i;
-  var random;
-  var char;
-
-  if (this.active) {
-    window.requestAnimationFrame(this.animate.bind(this));
-
-    var indexes = [];
-
-    for (i = 0; i < this.letters.length; i++) {
-      var current = this.letters[i];
-
-      if (!current.isDead) {
-        random = Math.floor(Math.random() * (this.alphabet.length - 0));
-        char = this.alphabet[random];
-        current.render(char);
-      } else {
-        indexes.push(i);
-      }
-    }
-
-    for (i = 0; i < indexes.length; i++) {
-      this.letters.splice(indexes[i], 1);
-    }
-
-    if (this.letters.length == 0) {
-      this.stop();
-    }
-  }
-};
-
-Letters.prototype.start = function (word) {
-  this.init(word);
-};
-
-Letters.prototype.stop = function () {
-  this.active = false;
-};
-
-Letter = function (DOMElement, nextChar) {
-  var scope = this;
-
-  this.DOMEl = DOMElement;
-  this.char = DOMElement.innerHTML;
-  this.next = nextChar;
-  this.speed = Math.floor(Math.random() * (300 - 50));
-  this.total = 0;
-  this.duration = 2000;
-  this.animating = true;
-  this.isDead = false;
-
-  this.timer = setInterval(function () {
-    if (scope.animating === true) {
-      scope.total += scope.speed;
-    }
-    scope.animating = !scope.animating;
-  }, this.speed);
-
-  this.animate();
-
-  return this;
-};
-
-Letter.prototype.animate = function () {
-  var i;
-  var random;
-
-  if (!this.isDead) {
-    window.requestAnimationFrame(this.animate.bind(this));
-  }
-
-  if (this.total < this.duration) {
-    if (this.animating) {
-      this.DOMEl.innerHTML = this.char;
-    }
-  } else {
-    this.isDead = true;
-
-    if (!this.next) {
-      var parent = document.getElementById("word");
-      parent.removeChild(this.DOMEl);
-      return;
-    }
-
-    this.DOMEl.innerHTML = this.next;
-  }
-};
-
-Letter.prototype.render = function (char) {
-  if (!this.animating) {
-    this.char = char;
-  }
-};
-
-var clientWidth = document.getElementById("center").clientWidth;
-if (clientWidth <= 640) {
-  var word = ["js", "Git", "npm", "...", "DC", "Sql"];
-} else {
-  var word = ["React", "Node.js", "MongoDb", "GitHub", "Next.js", "Batman"];
-}
-
-console.log(clientWidth);
-
-var nextWord = 1;
-
-var letters = new Letters();
-
-setTimeout(function () {
-  letters.start(word[nextWord]);
-
-  setInterval(function () {
-    nextWord++;
-    if (nextWord >= word.length) nextWord = 0;
-
-    letters.start(word[nextWord]);
-  }, 10000);
-}, 2000);
--- js/main.js
+++ js/main.js
@@ -1,21 +1,70 @@
-//NavBar shrink
-// const navbar = document.querySelector(".navcontainer");
-// const logo = document.querySelector(".logo");
+window.addEventListener("load", () => {
+  document.querySelector(".main").classList.remove("hidden");
+  document.querySelector(".nav").classList.remove("hidden");
+  // document.querySelector(".home-section").classList.add("active");
+  // ---------------Page lOader -----------
+  document.querySelector(".page-loader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".page-loader").style.display = "none";
+  }, 1000);
+});
 
-// window.onscroll = () => {
-//   if (
-//     document.body.scrollTop > 100 ||
-//     document.documentElement.scrollTop > 100
-//   ) {
-//     navbar.style.height = "60px";
-//     navbar.style.fontSize = "14px";
-//     logo.classList.add("logo-small");
-//   } else {
-//     navbar.style.height = "100px";
-//     navbar.style.fontSize = "16px";
-//     logo.classList.remove("logo-small");
-//   }
-// };
+const tabsContainer = document.querySelector(".about-tabs"),
+  aboutSection = document.querySelector("#about");
+
+tabsContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("tab-item") &&
+    !e.target.classList.contains("active")
+  ) {
+    tabsContainer.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    const target = e.target.getAttribute("data-target");
+    // console.log(target);
+    aboutSection
+      .querySelector(".tab-content.active")
+      .classList.remove("active");
+    aboutSection.querySelector(target).classList.add("active");
+  }
+});
+
+// Portfolio PopUP -------------------------------
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    togglePortfolioPopup();
+    document.querySelector(".portfolio-popup").scrollTo(0, 0);
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+function togglePortfolioPopup() {
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  document.querySelector(".main").classList.toggle("fade-out");
+  document.querySelector(".nav").classList.toggle("fade-out");
+}
+document
+  .querySelector(".pp-close")
+  .addEventListener("click", togglePortfolioPopup);
+
+// hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("pp-inner")) {
+    togglePortfolioPopup();
+  }
+});
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(
+    ".portfolio-item-thumbnail img"
+  ).src;
+
+  document.querySelector(".pp-header h3").innerHTML =
+    portfolioItem.querySelector(".portfolio-item h3").innerHTML;
+
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(
+    ".portfolio-item-details"
+  ).innerHTML;
+}
 
 //NavBar Hamburger
 const navSlide = () => {
@@ -94,99 +143,3 @@ const tween2Mobile2 = KUTE.fromTo(
 
 tween1Mobile2.start();
 tween2Mobile2.start();
-
-(function () {
-  var quotes = $(".set1");
-  var quoteIndex = -1;
-
-  function showNextQuote() {
-    ++quoteIndex;
-    quotes
-      .eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
-  }
-
-  showNextQuote();
-})();
-
-(function () {
-  var quotes = $(".set2");
-  var quoteIndex = -1;
-
-  function showNextQuote() {
-    ++quoteIndex;
-    quotes
-      .eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
-  }
-
-  showNextQuote();
-})();
-
-(function () {
-  var quotes = $(".set3");
-  var quoteIndex = -1;
-
-  function showNextQuote() {
-    ++quoteIndex;
-    quotes
-      .eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
-  }
-
-  showNextQuote();
-})();
-
-(function () {
-  var quotes = $(".g1");
-  var quoteIndex = -1;
-
-  function showNextQuote() {
-    ++quoteIndex;
-    quotes
-      .eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
-  }
-
-  showNextQuote();
-})();
-
-(function () {
-  var quotes = $(".g2");
-  var quoteIndex = -1;
-
-  function showNextQuote() {
-    ++quoteIndex;
-    quotes
-      .eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
-  }
-
-  showNextQuote();
-})();
-
-(function () {
-  var quotes = $(".g3");
-  var quoteIndex = -1;
-
-  function showNextQuote() {
-    ++quoteIndex;
-    quotes
-      .eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
-  }
-
-  showNextQuote();
-})();
